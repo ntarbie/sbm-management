2e0a3559288f252ec22519be67f338917b17071b
Nate Tarbutton <ntarbutton@Nate.local>
2021-06-17 12:05:49 -0700

    SBM Management

--- src/components/slices/Quote.js
+++ src/components/slices/Quote.js
@@ -6,7 +6,7 @@ const Quote = ({ slice }) => (
   <section className="p-8 quote-slice">
     <div className="max-w-screen-lg mx-auto flex flex-col items-center">
       <blockquote className="italic font-black px-12 border-l-4 border-gray-300 relative text-lg w-full">{<RichText render={slice.primary.quote.raw}/>}
-      {slice.primary.citation && <cite class="block text-primary mt-2 text-base font-normal uppercase">{RichText.asText(slice.primary.citation.raw)} {slice.primary.job_title.raw.length > 0 && (" - " + RichText.asText(slice.primary.job_title.raw))}</cite>}
+      {slice.primary.citation && <cite class="block text-primary mt-2 text-base font-normal uppercase">{RichText.asText(slice.primary.citation.raw)} {slice.primary.job_title.raw && slice.primary.job_title.raw.length > 0 && (" - " + RichText.asText(slice.primary.job_title.raw))}</cite>}
       </blockquote>
     </div>
   </section>
